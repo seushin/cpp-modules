31c453d5a0a57b1ee5566c22e27de01b4973a7be
seushin <seushin@student.42seoul.kr>
2022-05-16 14:08:25 +0900

    Refactor main.cpp in cpp02/03

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -10,6 +10,19 @@ static void expect(bool a, bool b)
 	}
 }
 
+static bool _bsp(const Point a, const Point b, const Point c, const Point p)
+{
+	static int count;
+	bool res = bsp(a, b, c, p);
+
+	std::cout << "case" << count++ << ": "
+			  << p << " in "
+			  << a << ", " << b << ", " << c
+			  << " area? " << (res ? "true" : "false") << std::endl;
+
+	return (res);
+}
+
 int main()
 {
 	{
@@ -20,15 +33,15 @@ int main()
 		Point p(0.5f, 0.5f);
 		Point q(0.5f, 1.5f);
 
-		expect(bsp(a, b, c, p), true);
-		expect(bsp(b, a, c, p), true);
-		expect(bsp(c, a, b, p), true);
-		expect(bsp(c, b, a, p), true);
+		expect(_bsp(a, b, c, p), true);
+		expect(_bsp(b, a, c, p), true);
+		expect(_bsp(c, a, b, p), true);
+		expect(_bsp(c, b, a, p), true);
 
-		expect(bsp(a, b, c, q), false);
-		expect(bsp(b, a, c, q), false);
-		expect(bsp(c, a, b, q), false);
-		expect(bsp(c, b, a, q), false);
+		expect(_bsp(a, b, c, q), false);
+		expect(_bsp(b, a, c, q), false);
+		expect(_bsp(c, a, b, q), false);
+		expect(_bsp(c, b, a, q), false);
 	}
 	{
 		Point b(0.0f, 0.0f);
@@ -38,15 +51,15 @@ int main()
 		Point p(1.0f, 1.0f);
 		Point q(2.0f, 2.0f);
 
-		expect(bsp(a, b, c, p), true);
-		expect(bsp(b, a, c, p), true);
-		expect(bsp(c, a, b, p), true);
-		expect(bsp(c, b, a, p), true);
+		expect(_bsp(a, b, c, p), true);
+		expect(_bsp(b, a, c, p), true);
+		expect(_bsp(c, a, b, p), true);
+		expect(_bsp(c, b, a, p), true);
 
-		expect(bsp(a, b, c, q), false);
-		expect(bsp(b, a, c, q), false);
-		expect(bsp(c, a, b, q), false);
-		expect(bsp(c, b, a, q), false);
+		expect(_bsp(a, b, c, q), false);
+		expect(_bsp(b, a, c, q), false);
+		expect(_bsp(c, a, b, q), false);
+		expect(_bsp(c, b, a, q), false);
 	}
 	return (0);
 }
